7643faec86ca64e4840fcd7df599203f230532d4
Ramalingam <Keerthi.Ramalingam@britishgas.co.uk>
2018-01-12 10:44:43 +0000

    added customized bootstrap

--- Azure/vmss/Windows/customScript/custom.ps1
+++ Azure/vmss/Windows/customScript/custom.ps1
@@ -1,9 +1,10 @@
 param (
-    [string]$parm = ""
+    [string]$parm1 = "",
+    [string]$parm2 = ""
  )
 
 Add-Content C:\tst\load.ps1 "@echo off"
 Add-Content C:\tst\load.ps1 ":loop"
 Add-Content C:\tst\load.ps1 "goto loop"
 
-Add-Content C:\tst\load.ps1 $parm
+Add-Content C:\tst\load.ps1 $parm1 $parm2
