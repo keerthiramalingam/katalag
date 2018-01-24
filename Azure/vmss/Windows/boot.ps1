cedd7e73b7f382c7ea4b4a308fa42b67a1957764
Ramalingam <Keerthi.Ramalingam@britishgas.co.uk>
2018-01-24 15:26:54 +0000

    stripped artifact repostory

--- Azure/vmss/Windows/boot.ps1
+++ Azure/vmss/Windows/boot.ps1
@@ -2,6 +2,11 @@ param (
     [string]$pp_region = "region not passed",
     [string]$pp_environment = "env  not passed",
     [string]$pp_role = "role not passed",
+    [string]$vmss = ""
  )
 
- Add-Content C:\PerfLogs\output.txt "$(Get-Date) $pp_region XXXXX $pp_environment XXXXX $pp_role"
+ Add-Content C:\tst\output.txt "$(Get-Date) $pp_region XXXXX $pp_environment XXXXX $pp_role and $vmss"
+
+ Invoke-Expression $vmss
+
+ Add-Content C:\tst\output.txt "done $vmss"
--- Azure/vmss/Windows/custom.ps1
+++ Azure/vmss/Windows/custom.ps1
@@ -1,3 +1,9 @@
-Add-Content C:\PerfLogs\load.ps1 "@echo off"
-Add-Content C:\PerfLogs\load.ps1 ":loop"
-Add-Content C:\PerfLogs\load.ps1 "goto loop"
+param (
+    [string]$parm = ""
+ )
+
+Add-Content C:\tst\load.ps1 "@echo off"
+Add-Content C:\tst\load.ps1 ":loop"
+Add-Content C:\tst\load.ps1 "goto loop"
+
+Add-Content C:\tst\load.ps1 $parm
